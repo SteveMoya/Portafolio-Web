2fd0fbac2d6a025872b4782a0f40036e169431b1
SteveMoya <100615052@est.uasd.edu.do>
2023-10-16 20:55:30 -0400

    feat: Agregar componentes y elementos estaticos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stevemoya.me',
-  integrations: [mdx(), sitemap(), tailwind(), ],
+  integrations: [mdx(), sitemap(), tailwind(), partytown()],
   //Static is default
-  output:"static"
+  output: "static"
 });
