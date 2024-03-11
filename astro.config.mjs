64fed683665155eeb212c95b76cb880a215fec13
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-03-11 16:56:22 -0400

    refactor: :recycle: Refactorizacion del codigo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
+
+// import devtoolbarTailwind from "astro-devtoolbar-tailwind";
+// import lighthouse from 'astro-lighthouse';
+
+// import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stevemoya.me',
-  integrations: [mdx(), sitemap(), tailwind(), partytown()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown(), icon(), robotsTxt(),
+  // devtoolbarTailwind(), 
+  // lighthouse(),
+  //  metaTags()
+],
   //Static is default
   output: "static"
 });
