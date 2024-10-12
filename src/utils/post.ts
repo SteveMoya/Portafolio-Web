3a9d54537776863d87ac5ef000a566d84aff6537
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-10-12 05:13:34 -0400

    fix: :bug: Correcion de bug que se mostraban blog, categorias y tags de los blog en borrador

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,7 +1,10 @@
-import { getCollection } from 'astro:content'
+import { getCollection, type CollectionEntry } from 'astro:content'
+
+type relatedPostsType = CollectionEntry<'blog'>
+
 
 export const getCategories = async () => {
-	const posts = await getCollection('blog')
+	const posts = await getPosts()
 	const categories = new Set(posts.map((post) => post.data.category))
 	return Array.from(categories)
 }
@@ -14,8 +17,19 @@ export const getPosts = async (max?: number) => {
 		.slice(0, max)
 }
 
+export const getRelatedPosts = async (post: relatedPostsType) => {
+	const lowercaseTags = post.data.tags.map((tag) => tag.toLowerCase())
+	const posts = await getPosts()
+	const relatedPosts = posts.filter(
+		(p) =>
+			p.slug !== post.slug &&
+			p.data.tags.some((t) => lowercaseTags.includes(t.toLowerCase()))
+	)
+	return relatedPosts.slice(0, 3)
+}
+
 export const getTags = async () => {
-	const posts = await getCollection('blog')
+	const posts = await getPosts()
 	const tags = new Set()
 	posts.forEach((post) => {
 		post.data.tags.forEach((tag) => {
