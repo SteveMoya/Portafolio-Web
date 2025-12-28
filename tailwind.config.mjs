96593d4cc91aa9c1b11673ce675a80caa41bb043
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2025-12-27 20:27:13 -0400

    feat: add CV components and layout structure
    - Introduced new CV layout and sections including About, Education, Experience, Hero, Projects, Skills, and Tail.
    - Created TypeScript definitions for CV structure and related interfaces.
    - Added various SVG icons for social media and technologies.
    - Implemented a theme switcher and keyboard manager for enhanced user experience.
    - Enhanced styling for print and screen display.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,15 @@ import { addDynamicIconSelectors } from '@iconify/tailwind';
 import scrollDrivenAnimations from "@adam.plesnik/tailwindcss-scroll-driven-animations"
 // WEBSITE: https://scrolldriven.dev/
 
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -15,6 +24,37 @@ export default {
 				monoton : ['Monoton', 'cursive', defaultTheme.fontFamily.mono],
 				poppins : ['Poppins', 'sans-serif', defaultTheme.fontFamily.sans],
 				raleway : ['Raleway', 'sans-serif', defaultTheme.fontFamily.sans],
+			},
+			textColor: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					muted: withOpacity("--color-text-muted"),
+					inverted: withOpacity("--color-text-inverted"),
+				},
+			},
+			backgroundColor: {
+				skin: {
+					fill: withOpacity("--color-fill"),
+					"button-accent": withOpacity("--color-button-accent"),
+					"button-accent-hover": withOpacity("--color-button-accent-hover"),
+					"button-muted": withOpacity("--color-button-muted"),
+				},
+			},
+			colors: {
+				skin: {
+					hue: withOpacity("--color"),
+					muted: withOpacity("--muted"),
+				},
+			},
+			ringColor: {
+				skin: {
+					fill: withOpacity("--color-fill"),
+				},
+			},
+			gradientColorStops: {
+				skin: {
+					hue: withOpacity("--color-fill"),
+				},
 			},
 		},
 	},
